ac6a5b02708ad12e5bbeeb6a0aea632fc484f270
Pi3rr3 4prisium <159861323+Pi3rr3-aprisium@users.noreply.github.com>
2025-06-11 14:21:12 +0800

    echo bot test with azure services

---
+++ MonBotCSharp/Bots/EchoBot.cs
@@ -0,0 +1,30 @@
+﻿// Generated with EchoBot .NET Template version v4.22.0
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace EchoBot.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Text($"Agent: {turnContext.Activity.Text}"), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello welcome to you sinner";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
